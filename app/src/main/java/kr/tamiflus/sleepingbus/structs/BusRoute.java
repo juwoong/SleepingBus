44bda037664c34b58ffa00192282591063d4c49d
김정욱 <yhn523@naver.com>
2016-08-12 11:27:47 +0900

    BusRoute class - not finished

---
+++ app/src/main/java/kr/tamiflus/sleepingbus/structs/BusRoute.java
@@ -0,0 +1,16 @@
+package kr.tamiflus.sleepingbus.structs;
+
+import java.util.GregorianCalendar;
+
+/**
+ * Created by 김정욱 on 2016-08-12.
+ */
+public class BusRoute {
+    private String number;
+    private String id;
+    private String type;
+    private String locate;
+    private int[] arriveTime = new int[2];
+
+
+}

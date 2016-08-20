1f2c26afc20206864cc8d857daea60c51c7c5dfd
김정욱 <yhn523@naver.com>
2016-08-20 19:32:12 +0900

    busStationDBHelper를 통해 특정 정류장을 지나는 모든 노선 알아오기 완료

--- app/src/main/java/kr/tamiflus/sleepingbus/structs/BusRoute.java
+++ app/src/main/java/kr/tamiflus/sleepingbus/structs/BusRoute.java
@@ -6,8 +6,49 @@ import java.util.GregorianCalendar;
  * Created by 김정욱 on 2016-08-12.
  */
 public class BusRoute {
-    private String number;
-    private String id;
-    private String type;
-    private String locate;
+    private String regionName;
+    private String routeId;
+    private String routeName;
+    private String routeTypeCd;
+    private String routeTypeName;
+
+    public String getRegionName() {
+        return regionName;
+    }
+
+    public void setRegionName(String regionName) {
+        this.regionName = regionName;
+    }
+
+    public String getRouteId() {
+        return routeId;
+    }
+
+    public void setRouteId(String routeId) {
+        this.routeId = routeId;
+    }
+
+    public String getRouteName() {
+        return routeName;
+    }
+
+    public void setRouteName(String routeName) {
+        this.routeName = routeName;
+    }
+
+    public String getRouteTypeCd() {
+        return routeTypeCd;
+    }
+
+    public void setRouteTypeCd(String routeTypeCd) {
+        this.routeTypeCd = routeTypeCd;
+    }
+
+    public String getRouteTypeName() {
+        return routeTypeName;
+    }
+
+    public void setRouteTypeName(String routeTypeName) {
+        this.routeTypeName = routeTypeName;
+    }
 }

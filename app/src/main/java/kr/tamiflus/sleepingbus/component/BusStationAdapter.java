9fd293bfc1a884373cc9e6d6d96fbd7a1be83b05
김정욱 <yhn523@naver.com>
2016-08-21 17:33:28 +0900

    BusStationAdapter

--- app/src/main/java/kr/tamiflus/sleepingbus/component/BusStationAdapter.java
+++ app/src/main/java/kr/tamiflus/sleepingbus/component/BusStationAdapter.java
@@ -1,6 +1,7 @@
 package kr.tamiflus.sleepingbus.component;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,11 +10,14 @@ import android.view.ViewGroup;
 import java.util.ArrayList;
 import java.util.List;
 
+import kr.tamiflus.sleepingbus.GetBusStationIDActivity;
 import kr.tamiflus.sleepingbus.R;
+import kr.tamiflus.sleepingbus.StationInfoActivity;
 import kr.tamiflus.sleepingbus.holders.BusStationViewHolder;
 import kr.tamiflus.sleepingbus.holders.SectionViewHolder;
 import kr.tamiflus.sleepingbus.structs.BusStation;
 import kr.tamiflus.sleepingbus.structs.BusStationTag;
+import kr.tamiflus.sleepingbus.utils.BusStationToStrArray;
 
 /**
  * Created by tamiflus on 2016. 7. 31..
@@ -68,7 +72,7 @@ public class BusStationAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
             holder.sectionName.setText(tag.locate);
         }
         else{
-            BusStation st = list.get(position);
+            final BusStation st = list.get(position);
             BusStationViewHolder holder = (BusStationViewHolder) vh;
 
             //TODO: holder.layout에 onClickListener 달 것
@@ -76,6 +80,15 @@ public class BusStationAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
             holder.nameView.setText(st.getName());
             holder.idView.setText(st.getId());
             holder.locationView.setText(st.getRegion());
+            holder.layout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context, StationInfoActivity.class);
+                    intent.putExtra("departStation", BusStationToStrArray.listToArr(st));
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startActivity(intent);
+                }
+            });
         }
     }
 
